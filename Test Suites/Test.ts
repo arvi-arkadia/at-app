cefd65f1a88a529d96e92ffde84a83db43ef6ae1
arvi-arkadia <120647001+arvi-arkadia@users.noreply.github.com>
2025-05-31 20:00:54 +0700

    [add] New File

---
+++ Test Suites/Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>80655b99-67cb-44a9-b564-59d2480615eb</testSuiteGuid>
+</TestSuiteEntity>
